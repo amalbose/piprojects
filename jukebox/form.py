a5d32f185a95876a7feb72297ac012f44a4f3946
Amal Bose <axatrikx@gmail.com>
2014-02-22 12:13:27 +0530

    Added Files page

--- jukebox/form.py
+++ jukebox/form.py
@@ -16,39 +16,39 @@ class myHandler(BaseHTTPRequestHandler):
         if self.path=="/":
             self.path="/index_example3.html"
 
-        try:
-            #Check the file extension required and
-            #set the right mime type
+            try:
+                #Check the file extension required and
+                #set the right mime type
 
-            sendReply = False
-            if self.path.endswith(".html"):
-                mimetype='text/html'
-                sendReply = True
-            if self.path.endswith(".jpg"):
-                mimetype='image/jpg'
-                sendReply = True
-            if self.path.endswith(".gif"):
-                mimetype='image/gif'
-                sendReply = True
-            if self.path.endswith(".js"):
-                mimetype='application/javascript'
-                sendReply = True
-            if self.path.endswith(".css"):
-                mimetype='text/css'
-                sendReply = True
+                sendReply = False
+                if self.path.endswith(".html"):
+                    mimetype='text/html'
+                    sendReply = True
+                if self.path.endswith(".jpg"):
+                    mimetype='image/jpg'
+                    sendReply = True
+                if self.path.endswith(".gif"):
+                    mimetype='image/gif'
+                    sendReply = True
+                if self.path.endswith(".js"):
+                    mimetype='application/javascript'
+                    sendReply = True
+                if self.path.endswith(".css"):
+                    mimetype='text/css'
+                    sendReply = True
 
-            if sendReply == True:
-                #Open the static file requested and send it
-                f = open(curdir + sep + self.path) 
-                self.send_response(200)
-                self.send_header('Content-type',mimetype)
-                self.end_headers()
-                self.wfile.write(f.read())
-                f.close()
-            return
+                if sendReply == True:
+                    #Open the static file requested and send it
+                    f = open(curdir + sep + self.path) 
+                    self.send_response(200)
+                    self.send_header('Content-type',mimetype)
+                    self.end_headers()
+                    self.wfile.write(f.read())
+                    f.close()
+                return
 
-        except IOError:
-            self.send_error(404,'File Not Found: %s' % self.path)
+            except IOError:
+                self.send_error(404,'File Not Found: %s' % self.path)
 
         elif self.path=="/Files":
             self.send_response(200)
